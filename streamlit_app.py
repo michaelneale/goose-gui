1b597c26c83e20b820e4b4d906b65250b2a2bf86
Mic Neale <michael.neale@gmail.com>
2024-10-14 16:04:41 +1100

    Add Streamlit app and .goosehints file with guide for validation and running.

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,6 @@ def main():
 
     output_placeholder = st.empty()
 
-    session_state = st.session_state
-
     if 'process' not in session_state:
         session_state.process = None
 
@@ -36,58 +34,17 @@ def main():
         session_state.process.stdin.flush()
 
         # Capture and stream output after sending each command
-        output = []
         while True:
             line = session_state.process.stdout.readline()
             if not line and session_state.process.poll() is not None:
                 break
             if line:
-                output.append(line)
-                output_placeholder.markdown("".join(output))
+                session_state.output.append(line)
+                output_placeholder.markdown("".join(session_state.output))
 
         error_line = session_state.process.stderr.readline()
         if error_line:
             output_placeholder.error(error_line)
-        if not os.path.isdir(directory):
-            st.error("Invalid directory")
-            return
-        os.chdir(directory)
-
-        # Start a shell session using 'goose session start'
-        process = subprocess.Popen('goose session start', shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-
-        # Send user commands and get the results in real time
-        if user_command:
-            process.stdin.write(user_command + '\n')
-            process.stdin.flush()
-
-            # Capture and stream output after sending each command
-            session_state.output.extend(output)
-
-            while True:
-                line = process.stdout.readline()
-                if not line and process.poll() is not None:
-                    break
-                if line:
-                    output.append(line)
-                    output_placeholder.markdown("".join(output))
-
-            error_line = process.stderr.readline()
-            if error_line:
-                output_placeholder.error(error_line)
-
-        output = []
-        while True:
-            if process.poll() is not None:
-                break
-            line = process.stdout.readline()
-            if line:
-                output.append(line)
-                output_placeholder.markdown("".join(output))
-        process.stdout.close()
-        process.wait()
-
-        output_placeholder.info("Session ended. Add more input to continue or restart the session.")
 
 if __name__ == "__main__":
     main()
